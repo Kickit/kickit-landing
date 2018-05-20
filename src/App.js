a3a7c77565bf365650b5ef46a445a798f1c96865
Foopert <foopert@gmail.com>
2018-05-20 16:24:36 -0400

    Add: app.js now includes UI circles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="circle one">&nbsp;</div>
+        <div className="circle two">&nbsp;</div>
+        <div className="circle three">&nbsp;</div>
+        <div className="circle four">&nbsp;</div>
+        <div className="circle five">&nbsp;</div>
+        <div className="circle six">&nbsp;</div>
+        <div className="circle seven">&nbsp;</div>
+        <div className="circle eight">&nbsp;</div>
+        <div className="circle nine">&nbsp;</div>
+        <div className="circle ten">&nbsp;</div>
+        <div className="circle eleven">&nbsp;</div>
+        <div className="circle twelve">&nbsp;</div>
+        <div className="circle thirteen">&nbsp;</div>
+        <div className="circle fourteen">&nbsp;</div>
       </div>
     );
   }
