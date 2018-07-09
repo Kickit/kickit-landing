a3a1caf23fc8f255463c6c2b7bfe7dc3480cce5f
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-07-09 11:50:55 -0400

    Add: breakout nav bar into componet (incomplete)

---
+++ src/app/components/nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Input, Form } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+class Nav extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div className="home">
+
+      </div>
+    )
+  }
+
+}
+export default Nav;
