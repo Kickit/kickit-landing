a9057cc1ecc68151214c6ce25e7d9bcf0d421152
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-08-08 18:42:30 -0400

    Add: update home route to be used in layout

--- src/app/Home/Home.js
+++ src/app/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Button, Input, Form } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { Layout, Menu, Icon } from 'antd';
 import logo from '../../resources/header_k.png'
-import Header from './HomeHeader'
+import MyHeader from './HomeHeader'
 import Github from './HomeGithub'
 import Contribute from './HomeContribute'
 import UseNow from './HomeUseNow'
+
+const { Header, Content, Footer, Sider } = Layout;
 
 class Home extends React.Component {
   constructor () {
@@ -18,21 +21,15 @@ class Home extends React.Component {
     return (
       <div className="home">
         <div className='slanted'>
-          <div className='nav'>
-            {/*<Link to='/docs'><Button basic target="_blank" color='blue'>Docs</Button></Link>*/}
-            <Button basic href="https://app.kickit.us/" target="_blank" color='blue'>Login</Button>
-            <Button href="https://app.kickit.us/" target="_blank" color='yellow'>Register</Button>
-          </div>
-        
         <img className="logo" src={logo} height={"200px"} alt={""}/>
         <UseNow/>
         </div>
-        <Header/>
+        <MyHeader/>
         <Github/>
         <Contribute/>
       </div>
     )
   }
-
 }
+
 export default Home;
