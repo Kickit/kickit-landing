d3dd9b6c78d87e7741f3834623d3977890a35f67
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-08-08 18:43:43 -0400

    Add: init doc links

---
+++ src/documentation/docs.js
@@ -0,0 +1,14 @@
+export default {
+  installation: {
+    gettingStarted: "https://gist.githubusercontent.com/NickLewanowicz/35c637dcd2f53b763b5aa3d303adb5e6/raw/e0968346ea50e7666fe33e7bab0ab5d3ad1a7001/Kickit_gettingStarted",
+  },
+  howToContribute: {
+    devEnvironment: ""
+  },
+  APIReference: {
+    graphQL: ""
+  },
+  // FAQ: {
+  // },
+
+}
